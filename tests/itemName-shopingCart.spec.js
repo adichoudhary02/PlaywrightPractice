8df624b0ec893aaec03e804ffa73be0300c2ac83
aditya-sdettech <aditya.choudhary@sdettech.com>
2026-03-09 12:24:08 +0530

    Created a testcases to check and add items to the cart

---
+++ tests/itemName-shopingCart.spec.js
@@ -0,0 +1,54 @@
+import {test, expect} from '@playwright/test'
+
+test('login page',async({page}) => {
+
+    //Go to the saucedemo website
+    await page.goto('https://www.saucedemo.com/');
+
+    //Check for the title
+    await expect(page).toHaveTitle('Swag Labs');
+
+    //SwagLabs Text
+    await expect(page.getByText('Swag Labs')).toBeVisible();
+      
+    //UserName field
+    await page.getByPlaceholder('Username').fill('standard_user');
+
+    //Password field
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    
+    //Login button
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page).toHaveURL(/inventory/);
+    await expect(page.getByText('Products')).toBeVisible();
+
+    await expect(page.locator('[data-test = "inventory-list"]')).toBeVisible();
+
+    const items = page.locator('[data-test="inventory-item"]');
+
+    let itemCount = 0;
+    const count = await items.count();
+    for (let i = 0; i < count; i++) {
+        console.log(`---------(ITEM NO: ${i+1})---------`)
+        let item = items.nth(i);
+        let item_name = await item.locator('[data-test="inventory-item-name"]').textContent();
+        console.log(`Item : ${item_name}`);
+
+        let item_desc = await item.locator('[data-test="inventory-item-desc"]').textContent();
+        console.log(`Desc : ${item_desc}`);
+
+        let item_price = await item.locator('[data-test="inventory-item-price"]').textContent();
+        console.log(`Price : ${item_price}`);
+
+        await item.locator("[data-test^='add-to-cart-']").click();
+        itemCount++;
+        
+        let itemInCart = await page.locator('[data-test="shopping-cart-badge"]').textContent();
+        if (itemInCart == itemCount) console.log("Cart Badge number matched : "+ itemInCart);
+       
+     }
+
+     
+    await page.waitForTimeout(4000);
+});
